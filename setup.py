0d4717007485caf30bb0a0ead7c7119962274553
Alexey Shamrin <alexey.shamrin@futurice.com>
2017-12-02 12:40:59 +0200

    allow importing the python module, fixes #8

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name='namesgenerator',
-    version='0.2',
+    version='0.3',
     scripts=['namesgenerator.py'],
+    py_modules=['namesgenerator'],
     url='https://github.com/shamrin/namesgenerator',
     maintainer='jznf',
     maintainer_email='bofh@zvsx.net',
